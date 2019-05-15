df92d3c9454421d1550f7f192af6d8687cb58f80
Stefan Haslinger <stefan.haslinger@informatom.com>
2019-05-15 20:50:16 +0200

    minimal wrapper is working

--- example/wikidata_client_example.dart
+++ example/wikidata_client_example.dart
@@ -1,6 +1,16 @@
 import 'package:wikidata_client/wikidata_client.dart';
 
-main() {
-  var query = SparqlQuery();
-  print(SparqlQuery().run);
+Future<void> main() async {
+
+  String queryString = """
+SELECT ?item ?itemLabel 
+WHERE 
+{
+  ?item wdt:P31 wd:Q146.
+}
+""";
+
+  SparqlQuery query = SparqlQuery(queryString: queryString);
+  await query.run();
+  await print (query.result);
 }
--- lib/src/sparql_query.dart
+++ lib/src/sparql_query.dart
@@ -1,3 +1,27 @@
+import 'dart:io';
+import 'dart:convert';
+
 class SparqlQuery {
-  bool get run => true;
+  String queryString;
+  String result;
+
+  Future<void> run() async {
+    String encodedQueryString = Uri.encodeFull(queryString);
+    String requestUri = "https://query.wikidata.org/sparql?query=$encodedQueryString&format=json";
+
+    await HttpClient()
+      .getUrl(Uri.parse(requestUri))
+      .then((request) => request.close())
+      .then((response) {
+        response.transform(utf8.decoder).listen((contents) {
+          this.result = contents;
+        });
+      });
+
+    await null;
+  }
+
+  SparqlQuery({String queryString}) {
+    this.queryString = queryString;
+  }
 }
