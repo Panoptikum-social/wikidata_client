0e34b171262148eed380deaf407c13a97d2b2cfd
Stefan Haslinger <stefan.haslinger@informatom.com>
2019-05-15 19:03:22 +0200

    empty library template

---
+++ test/wikidata_client_test.dart
@@ -0,0 +1,16 @@
+import 'package:wikidata_client/wikidata_client.dart';
+import 'package:test/test.dart';
+
+void main() {
+  group('A group of tests', () {
+    Awesome awesome;
+
+    setUp(() {
+      awesome = Awesome();
+    });
+
+    test('First Test', () {
+      expect(awesome.isAwesome, isTrue);
+    });
+  });
+}
