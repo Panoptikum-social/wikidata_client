6f2745fa665722f987ef185df4138d5faa9bdfdc
stefan Haslinger <mail@stefan-haslinger.at>
2019-05-16 11:06:26 +0200

    properly running async with http pub

--- lib/src/sparql_query.dart
+++ lib/src/sparql_query.dart
@@ -1,24 +1,16 @@
-import 'dart:io';
 import 'dart:convert';
+import 'package:http/http.dart' as http;
 
 class SparqlQuery {
   String queryString;
-  String result;
 
-  Future<void> run() async {
+  run() async {
     String encodedQueryString = Uri.encodeFull(queryString);
-    String requestUri = "https://query.wikidata.org/sparql?query=$encodedQueryString&format=json";
+    Uri requestUri = Uri.parse("https://query.wikidata.org/sparql?query=$encodedQueryString&format=json");
 
-    await HttpClient()
-      .getUrl(Uri.parse(requestUri))
-      .then((request) => request.close())
-      .then((response) {
-        response.transform(utf8.decoder).listen((contents) {
-          this.result = contents;
-        });
-      });
-
-    await null;
+    http.Response response = await http.get(requestUri);
+    final json = jsonDecode(response.body);
+    return(json);
   }
 
   SparqlQuery({String queryString}) {
